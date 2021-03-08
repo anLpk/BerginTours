e722a5be700ec18f871bd59d097e6a6004ba0c3e
anil pak <anilpak35@gmail.com>
2021-03-08 19:16:14 +1100

    article controller and model changed according photos [] and article seed file added, also nav bar styled and bg color changed.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,16 @@
 class ArticlesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
   def index 
     @articles = Article.all
   end
 
   def show 
     @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :author, photos: [])
   end
 end
