a26ebf5402039e765ae68c1e0583afdb232f2f49
anil pak <anilpak35@gmail.com>
2021-03-14 17:40:15 +1100

    article model and controller updated regarding to tagabble

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,22 @@
 class ArticlesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
-  def index 
-    @articles = Article.all
+
+  def index
+    if params[:tag].present?
+      @articles = Article.tagged_with(params[:tag])
+    else
+      @articles = Article.all
+    end
   end
 
   def show 
     @article = Article.find(params[:id])
+    @related_articles = @article.find_related_tags
   end
 
   private
 
   def article_params
-    params.require(:article).permit(:title, :content, :author, photos: [])
+    params.require(:article).permit(:tag_list, :title, :content, :author, photos: [])
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
   has_many_attached :photos
+
+  acts_as_taggable_on :tags
 end
