501069e5436bacf8085157f6008176323925c1e3
anil pak <anilpak35@gmail.com>
2021-03-18 19:01:21 +1100

    session id added and stripe setup completed

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,4 +3,23 @@ class CartsController < ApplicationController
     @user = current_user
     @cart = Cart.find(params[:id])
   end
+
+  def update 
+    cart = Cart.find(params[:id])
+  
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [{
+        name: cart.id,
+        amount: cart.calculate_total_price,
+        currency: 'usd',
+        quantity: 1
+      }],
+      success_url: root_url,
+      cancel_url: cart_url(cart)
+    )
+  
+    cart.update(checkout_session_id: session.id)
+    redirect_to new_cart_payment_path(cart)
+  end
 end
