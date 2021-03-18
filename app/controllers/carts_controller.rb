23fd2cb40684038684474026bef1f1d1553c92e6
anil pak <anilpak35@gmail.com>
2021-03-18 19:44:57 +1100

    stripe config fixed and style completed. Now if cart is empty btn will be disabled and in the stripe payment section we able to display tour img

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,24 +2,25 @@ class CartsController < ApplicationController
   def show 
     @user = current_user
     @cart = Cart.find(params[:id])
-  end
 
-  def update 
-    cart = Cart.find(params[:id])
-  
+    if (@cart.calculate_total_price.to_i == 0)
+      @cart.update(checkout_session_id: nil)
+    else
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [{
-        name: cart.id,
-        amount: cart.calculate_total_price,
+        name: "Thanks for booking #{ @cart.id}",
+        images: @cart.bookings.map do |booking|
+          Cloudinary::Utils.cloudinary_url(booking.tour.photos[0].key)
+        end,
+        amount: @cart.calculate_total_price.to_i * 100,
         currency: 'usd',
         quantity: 1
       }],
       success_url: root_url,
-      cancel_url: cart_url(cart)
+      cancel_url: cart_url(@cart)
     )
-  
-    cart.update(checkout_session_id: session.id)
-    redirect_to new_cart_payment_path(cart)
+    @cart.update(checkout_session_id: session.id)
+    end
   end
 end
