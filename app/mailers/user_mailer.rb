3111ac22e85c3dbbf932f7c6684cb33d6a49ff06
anil pak <anilpak35@gmail.com>
2021-04-07 10:50:29 +1000

    user_mailer controller updated user instance variable added and mail to refer to user email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,9 @@
 class UserMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.welcome.subject
-  #
-  def welcome
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def welcome(user)
+    @user = params[:user] 
+    # Instance variable => available in view
+    
+    mail(to: @user.email, subject: 'Welcome to Bergin Tours')
+    # This will render a view in `app/views/user_mailer`!
   end
 end
